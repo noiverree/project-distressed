7c55a03949979967b72992af4a4b749faec6bbeb
Arucaden <daffamaulanasatria@gmail.com>
2023-02-07 15:07:47 +0700

    Adding transition menu

--- Assets/_Project/Scripts/Hunter/DetectionController.cs
+++ Assets/_Project/Scripts/Hunter/DetectionController.cs
@@ -47,7 +47,7 @@ namespace ProjectDistressed.Hunter
             }
         }
 
-
         #endregion
+        
     }
 }
---
+++ Assets/_Project/Scripts/MainMenu/LevelLoader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public void play()
+    {
+        LoadNextLevel();
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
+
